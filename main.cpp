f40a4105fab926d4058fef588b5cee8da407c006
WDCT-Wren <wrenctulio@gmail.com>
2026-07-18 23:59:18 +0800

    tests: tested the isFull() but still lacks testing in read/write bookkeeping

--- main.cpp
+++ main.cpp
@@ -52,9 +52,71 @@ class RingBuffer {
         bool isEmpty() const {
             return counter == 0;
         }
+
+        void printBuffer() {
+            for (int i = 0; i < arraySize; i++) {
+                std::cout << buffer[i] << " ";
+            }
+            std::cout << std::endl;
+        }
 } ring_buffer;
 
 int main() {
-    
+    int constexpr bufferSize = 64;
+
+    char data[bufferSize] = {
+        'i', '1', 'j', '5', '2', 'M', '*', 'w', 'Q', 'p', '^', '$', '@', '4', '_', '8',
+        '%', 'k', '3', '&', 'G', '!', '9', '7', 'x', 'V', 'z', 'D', '_', 'h', 'L', '=',
+        '0', '{', 'a', 'T', '[', 'u', 'Y', 'm', '|', '~', 'y', '9', '!', 'c', '2', 'b',
+        '4', 'L', '+', 'X', '6', 'P', '~', 'R', '9', '_', 'z', '4', '#', 'Q', '2', 'g'
+    };
+
+    char reciever[bufferSize] = {
+        'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e',
+        'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e',
+        'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e',
+        'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'
+    };
+
+    // push 64 bytes filling the system
+    for (int i = 0; i < bufferSize; i++) {
+        ring_buffer.push(data[i]);
+    }
+    std::cout << ring_buffer.isFull() << std::endl;
+
+    // pop 32 bytes
+    for (int i = 0; i < 32; i++) {
+        ring_buffer.pop(reciever[i]);
+    }
+
+    //display the popped 32 bytes
+    for (int i = 0; i < bufferSize; i++) {
+        std::cout << reciever[i] << " ";
+    }
+    std::cout << std::endl;
+
+    // push another 32 to wrap the tail around
+    for (int i = 0; i < 32; i++) {
+        ring_buffer.push(data[i]);
+    }
+    std::cout << ring_buffer.isFull() << std::endl;
+
+    // then pop the whole buffer.
+    std::cout << "popped buffer: " << std::endl;
+    for (int i = 0; i < bufferSize; i++) {
+        ring_buffer.pop(reciever[i]);
+    }
+
+    for (int i = 0; i < bufferSize; i++) {
+        std::cout << reciever[i] << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "real buffer: " << std::endl;
+    ring_buffer.printBuffer();
+
+    /*
+     * TODO: trace the expected pop sequence by hand.
+     */
     return 0;
 }
